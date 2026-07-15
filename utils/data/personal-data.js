d01ce152ba290efb8a3a06a64510a4a8e31d2560
Writtik Bhattacharya <90407907+WrittikGithub@users.noreply.github.com>
2026-07-15 17:15:09 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   shortName: "Writtik",
   profile: '/profilepic.png',
   designation: "Software Developer",
-  description: "I'm Writtik Bhattacharya, a passionate web and app developer with over 4 years of experience specializing in PHP, React, modern web technologies, and UI/UX design. I have a strong background in building secure, high-quality applications from scratch, leveraging the latest frameworks and best practices to deliver innovative solutions. My expertise spans responsive website development, mobile app creation, and crafting engaging digital experiences, always prioritizing performance, security, and effective user-centric design.",
+  description: "I'm Writtik Bhattacharya, a passionate web and app developer with around 4 years of experience specializing in PHP, React, modern web technologies, and UI/UX design. I have a strong background in building secure, high-quality applications from scratch, leveraging the latest frameworks and best practices to deliver innovative solutions. My expertise spans responsive website development, mobile app creation, and crafting engaging digital experiences, always prioritizing performance, security, and effective user-centric design.",
   email: 'rittik.2000.12@gmail.com',
   phone: '+91 8768-9792-61',
   address: 'Mysore, Karnataka, India - 570006',
